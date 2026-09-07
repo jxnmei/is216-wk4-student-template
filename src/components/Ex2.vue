d88dafb8ee8492870837600fc01419e5bba9eec4
jxnmei <janmei.gatchalian@gmail.com>
2026-09-07 09:36:26 +0800

    Exercise 1 and Exercise 2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -2,26 +2,22 @@
     import { ref } from 'vue'
 
     // Add code here
-    /*
-        Place and import images to your Vue component using one of the two methods below
-    
-    */
    const width = ref('200')
    const height = ref('200')
    const myImg = ref('/assets/me.png')
 
    // multiple attribute binding
-//    const obj = ref({
-//     width: '200',
-//     height: '200',
-//     src: '/src/assets/me.png'
-//    })
+   const obj = ref({
+    width: '200',
+    height: '200',
+    src: '/assets/me.png'
+   })
     
 </script>
 <template>
 
     <!-- Add code here -->
      <img v-bind:src="myImg" :width="width" :height="height"/>
-     <!-- <img v-bind="obj" /> -->
+     <img v-bind="obj" />
    
 </template>
