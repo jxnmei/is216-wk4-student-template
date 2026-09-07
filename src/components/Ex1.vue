1120163eecf3e71567c8071701125a868ce8f883
jxnmei <janmei.gatchalian@gmail.com>
2026-09-07 09:02:03 +0800

    Exercise 1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -2,7 +2,13 @@
     import { ref } from 'vue'
 
     // Add code here
-    // challenge: use array  
+    // challenge: use array 
+    const message = ref("Choose your favourite fruit : ")
+    const fruits = ref(["apple", "orange","peach"])
+
+    const fruit1 = fruits.value[0]
+    const fruit2 = fruits.value[1]
+    const fruit3 = fruits.value[2]
     
 </script>
 
